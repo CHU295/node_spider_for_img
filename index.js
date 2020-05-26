eddc8a4e7e95aa5c727b67f368f426e7dbec4463
储 <chu295@qq.com>
2020-05-26 10:20:45 +0800

    改为请求接口，获取b站美图

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 const superagent = require('superagent')
-const cheerio = require("cheerio")
 const fs = require("fs")
 
-superagent.get('https://www.acfun.cn/v/list123/index.htm').set({
-  // 'accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
-  // 'accept-encoding': 'gzip, deflate, br',
-  // 'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8',
-  // 'cache-control': 'no-cache',
-  // 'pragma': 'no-cache',
-  // 'referer': 'https://www.mzitu.com/',
-  // 'sec-fetch-mode': 'no-cors',
-  // 'sec-fetch-site': 'cross-site',
-  // 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+superagent.get('https://api.bilibili.com/x/space/arc/search?mid=72956117&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp').set({
+  'accept': 'application/json, text/plain, */*',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7',
+  'cookie': "buvid3=0C0FF40F-47B9-445E-6A4B-C7DB2184435C36409infoc; _uuid=9BC7E141-3663-B0A5-BB32-6F3B7A1293C938520infoc; bfe_id=1e33d9ad1cb29251013800c68af42315",
+  'origin': 'https://www.bilibili.com',
+  'referer': 'https://www.bilibili.com/v/music/perform/',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+  
 }).then(res => {
-  const $ = cheerio.load(res.text)
-  let el = $('body').find('img')
-
-  el.map((index, i) => {
-    let img_src = $(i).attr('data-original') || $(i).attr('src'),
-      news_title = $(i).attr('alt') || ''
-    if (!img_src) return
-    console.log(img_src, news_title)
-    superagent.get(img_src).pipe(fs.createWriteStream('./image/' + index + '.jpg'));
+  let data = JSON.parse(res.text)
+  let nums = data.data.list.vlist.length
+  let num1 = 0
+  let num2 = 0
+  data.data.list.vlist.map((i, index) => {
+    let url = 'http:' + i.pic // 文件地址
+    let name = url.split(/.+\//g)[1] // 获取的hash文件名，带文件格式后缀
+    let title = i.title // 原始文件名 新世纪福音战士 OP 残酷な天使のテーゼ 高橋洋子
+    let fileType = url.split(/.+\./g)[1] //文件后缀 .png
+    // name = title + '.' + fileType //如果需要输出文件名为原始的，打开这个注释。部分文件名带有特殊字符。可能会导致部分图片保存失败
+    superagent.get(url).timeout({
+      response: 5000,
+      deadline: 10000,
+    }).end((err, sres) => {
+      if (err) {
+        console.log('下载文件出错了，错误是：' + err);
+      }
+      else {
+        num1++
+        console.log(`下载进度:${num1}/${nums}`)
+        fs.writeFile('./image/' + name, sres.body, function (err) {
+          if (err) {
+            console.log('写文件出错了，错误是：' + err);
+          }
+          else {
+            num2++;
+            if (num2 === nums) {
+              console.log(`图片全部保存成功`);
+            } else {
+              console.log(`存储进度：${num2}/${nums}`);
+            }
+          }
+        })
+      }
+    })
   })
-
-  // fs.writeFile('./' + $('title').text() + '.html', res.text, () => { })
 }, function (error) {
   console.log(error)
 })
+
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 github:https://github.com/CHU295/node_spider_for_img
 # 使用方法
-npm i 
-
-npm start
+- 安装node，[官网地址](https://nodejs.org/zh-cn/)
+- npm i 
+- npm start
 
 # 说明
 引用了两个第三方插件
 1. superagent 一个轻量级、灵活的客户端请求代理模块
-2. cheerio nodejs的jq
+<!-- 2. cheerio nodejs的jq -->
 
 # 思路
-使用superagent访问网站，cheerio解析网页后拿到我们想要的东西
+<!-- 使用superagent访问网站，cheerio解析网页后拿到我们想要的东西 -->
 
-再使用superagent拿到文件流，最后写入到文件中
+<!-- 再使用superagent拿到文件流，最后写入到文件中 -->
 
 # 提醒
 部分网站存在限制，可自行尝试
